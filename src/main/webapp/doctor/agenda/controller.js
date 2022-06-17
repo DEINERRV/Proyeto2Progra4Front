99595a8450860d10ee9e7ef2fa23ce4cd4f08007
DEINERRV <deiner.ra10@gmail.com>
2022-06-16 21:46:25 -0600

    se agrego un poco de funcionalidad del calendario

--- src/main/webapp/doctor/agenda/controller.js
+++ src/main/webapp/doctor/agenda/controller.js
@@ -1,4 +1,6 @@
-
+var personas = new Array();
+var backend = "http://localhost:8080/ExpedienteMedicoBackEnd/api";
+const NET_ERR = 999;
 
 function genCalendario() {
     var x = 0;
@@ -21,11 +23,11 @@ function genCalendario() {
         $("#id-calendar").append(
           `<tr class="tr-calendar">
             <td>${i}:${xs}</td>
-            <td class="${horario[0].clase} no-events" id="${horario[0].id}" data-cita="1-${i}:${xs}" rowspan="1"></td>
-            <td class="${horario[1].clase} no-events" id="${horario[1].id}" data-cita="2-${i}:${xs}" rowspan="1"></td>
-            <td class="${horario[2].clase} no-events" id="${horario[2].id}" data-cita="3-${i}:${xs}" rowspan="1"></td>
-            <td class="${horario[3].clase} no-events" id="${horario[3].id}" data-cita="4-${i}:${xs}" rowspan="1"></td>
-            <td class="${horario[4].clase} no-events" id="${horario[4].id}" data-cita="5-${i}:${xs}" rowspan="1"></td>
+            <td class="${horario[0].clase} no-events" id="${horario[0].id}" data-cita="${i}:${xs}" data-dia="1" rowspan="1"></td>
+            <td class="${horario[1].clase} no-events" id="${horario[1].id}" data-cita="${i}:${xs}" data-dia="2" rowspan="1"></td>
+            <td class="${horario[2].clase} no-events" id="${horario[2].id}" data-cita="${i}:${xs}" data-dia="3" rowspan="1"></td>
+            <td class="${horario[3].clase} no-events" id="${horario[3].id}" data-cita="${i}:${xs}" data-dia="4" rowspan="1"></td>
+            <td class="${horario[4].clase} no-events" id="${horario[4].id}" data-cita="${i}:${xs}" data-dia="5" rowspan="1"></td>
           </tr>`
         );
 
@@ -41,8 +43,52 @@ function genCalendario() {
     
     $(".tr-calendar #cita").click(
     (e) => {
-      console.log(e.currentTarget.dataset.cita);
+      show(e.currentTarget.dataset.dia,e.currentTarget.dataset.cita);
     });
+}
+
+
+function agregarPersonas(){
+    personas.forEach((p)=>{
+        $('#persona').append(`
+        <option value="${p.id}" selected>${p.cedula}-${p.nombre}</option>
+        `)
+    });
+}
+
+function fetchAndList() {
+    const request = new Request(backend + '/personas/'+doctor.cedula, {method: 'GET', headers: {}});
+    (async () => {
+        try {
+            const response = await fetch(request);
+            if (!response.ok) {
+                errorMessage(response.status, $("#buscarDiv #errorDiv"));
+                return;
+            }
+            personas = await response.json();
+            agregarPersonas();
+        } catch (e) {
+            errorMessage(NET_ERR, $("#buscarDiv #errorDiv"));
+        }
+    })();
+}
+
+function show(dia,hora){
+    //agregar las personas al modal
+    fetchAndList();
+    //Abajo, obtener el hasta
+    var hor = parseInt(hora.split(':')[0]);
+    var min = parseInt(hora.split(':')[1])+doctor.tiempo;
+    if(min>=60){
+        hor++;
+        min= '00';
+    }
+    else if(min<10) min = '0' + min.toString();
+    //Agregar infor de la cita al modal
+    $('#info-cita').empty();
+    $('#info-cita').append('Para el '+dia+'  desde '+hora+' hasta '+hor+':'+min);
+    //mostrar el modal
+    $('#add-modal').modal('show');
 }
 
 function loaded() {
